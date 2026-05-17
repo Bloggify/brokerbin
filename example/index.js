befd46152bff6d8afc14943634a97aebae81ea7c
Ionică Bizău <bizauionica@gmail.com>
2026-05-17 11:42:08 +0300

    ES6 modules

--- example/index.js
+++ example/index.js
@@ -1,15 +1,15 @@
-const BrokerBinREST = require("../lib");
+import BrokerBinREST from "../lib/index.js";
 
 // REST Example
 (async () => {
     const bbClient = new BrokerBinREST({
         username: process.env.BROKERBIN_USERNAME || "your username",
         token: process.env.BROKERBIN_TOKEN || process.env.BROKERBIN_SOAP_KEY || "your token",
-    })
+    });
 
     const result = await bbClient.partSearch({
         query: "F238F",
     });
 
-    console.log(result)
-})().catch(console.error)
+    console.log(result);
+})().catch(console.error);
